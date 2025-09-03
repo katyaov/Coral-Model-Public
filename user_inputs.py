a43da897461f1e7d20aa57334dece467e3eb0331
Rio Button <43662515+riobutton@users.noreply.github.com>
2025-09-04 09:53:10 +1000

    restored config and utils - opts back in utils

--- user_inputs.py
+++ user_inputs.py
@@ -128,6 +128,8 @@ cyclone_years = {
 }
 
 # 8. Load Sediment Exposure
+# Typical clear water oligatrophic reef has suspended sediment < 5 mg/L (<15NTU) (Zweifler et al., 2024) and deposited sediment < 10 mg/cm^-2/day (Rogers 1990, Dutra et al., 2006, Falsarella et al., 2025), these can be used to inform the default baseline values.
+
 baseline_suspended_sediment = 5
 baseline_deposited_sediment = 10
 
